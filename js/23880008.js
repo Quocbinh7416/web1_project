6030a06693c8f42a2c620d60c8a72f7ec7f3d4eb
Quocbinh7416 <quocbinh7416@gmail.com>
2024-11-29 18:26:57 +0800

    recapcha

--- js/23880008.js
+++ js/23880008.js
@@ -1,5 +1,16 @@
 const API = 'https://web1-api.vercel.app/api';
 
+Handlebars.registerHelper('formatDate', function(date) {
+    let formatDate = new Date(date);
+    let options = {
+        weekday:'short',
+        year: 'numeric',
+        month: 'short',
+        year: 'numeric',
+        timeZoneName: 'short'
+    }
+    return formatDate.toLocaleString('en-US', options);
+})
 
 async function loadData(request, templateId, viewId) {
     const response = await fetch(`${API}/${request}`);
@@ -9,6 +20,26 @@ async function loadData(request, templateId, viewId) {
     var context = {data : data};
     var view = document.getElementById(viewId);
     view.innerHTML = template(context);
+}
+
+async function loadBlogs(request, currentPage = 1, templateId = 'blogs-template', viewId = 'blogs') {
+    const response = await fetch(`${API}/${request}?page=${currentPage}`);
+    var context = await response.json();
+    context.currentPage = currentPage;
+    context.request = request;
+
+    var source = document.getElementById(templateId).innerHTML;
+    var template = Handlebars.compile(source);
+
+    var view = document.getElementById(viewId);
+    view.innerHTML = template(context);
+}
+
+async function loadBlogsDetail(blogId, goToComments = false) {
+    await loadData(`blogs/${blogId}`, 'details-blogs-template', 'blogs');
+    if(goToComments) {
+        window.location.href = "#comments";
+    }
 }
 
 async function testData(request) {
